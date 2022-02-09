c62fb63b512308147035781967a2d00c1ade0a73
Ludo-Boa <ludo.boa.77@gmail.com>
2022-02-09 21:28:32 +0100

    initial commit

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse # Cette fonction est utilisée pour formater les URL
 
 # Create your models here.
 
@@ -12,7 +13,16 @@ class Genre(models.Model):
         return self.name
 
 
-from django.urls import reverse # Cette fonction est utilisée pour formater les URL
+class Language(models.Model):
+    """Modèle représentant une langue (ex. anglais, français, japonais, etc.)"""
+    name = models.CharField(verbose_name="Langue", max_length=200,
+                            help_text="Entrez la langue naturelle du livre (par exemple, anglais, français, japonais, etc.)")
+
+    def __str__(self):
+        """Chaîne pour représenter l'objet modèle (dans le site d'administration, etc.)"""
+        return self.name
+
+
 
 class Book(models.Model):
     """Cet objet représente un livre (mais ne traite pas les copies présentes en rayon)."""
@@ -45,16 +55,16 @@ import uuid # Ce module est nécessaire à la gestion des identifiants unique (R
 
 class BookInstance(models.Model):
     """Cet objet permet de modéliser les copies d'un ouvrage (C'est à dire, qui peut être emprunté)."""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
-    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
-    imprint = models.CharField(max_length=200)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='ID unique pour ce livre particulier dans toute la bibliothèque')
+    book = models.ForeignKey('Book', verbose_name="Livre", on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(verbose_name="Edition", max_length=200)
     due_back = models.DateField(verbose_name="Date retour", null=True, blank=True)
 
     LOAN_STATUS = (
-        ('m', 'Maintenance'),
-        ('o', 'On loan'),
-        ('a', 'Available'),
-        ('r', 'Reserved'),
+        ('m', 'Entretien'),
+        ('o', 'En prêt'),
+        ('a', 'Disponible'),
+        ('r', 'Reservé'),
     )
 
     status = models.CharField(
@@ -62,7 +72,7 @@ class BookInstance(models.Model):
         choices=LOAN_STATUS,
         blank=True,
         default='m',
-        help_text='Book availability',
+        help_text='Disponibilité du livre',
     )
 
     class Meta:
@@ -70,5 +80,24 @@ class BookInstance(models.Model):
 
     def __str__(self):
         """Fonction requise par Django pour manipuler les objets Book dans la base de données."""
-        return f'{self.id} ({self.book.title})'
+        return f'{self.book.title} ({self.id})'
+
+
+class Author(models.Model):
+    """Modèle représentant un auteur."""
+    first_name = models.CharField(verbose_name="Prénom", max_length=100)
+    last_name = models.CharField(verbose_name="Nom", max_length=100)
+    date_of_birth = models.DateField(verbose_name="Date de naissance", null=True, blank=True)
+    date_of_death = models.DateField('Décès', null=True, blank=True)
+
+    class Meta:
+        ordering = ['first_name','last_name']
+
+    def get_absolute_url(self):
+        """Renvoie l'url pour accéder à une instance d'auteur particulière."""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """Chaîne pour représenter l'objet Model."""
+        return f'{self.first_name} {self.last_name}'
 
